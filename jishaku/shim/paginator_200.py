dffb4fd12d1acad6947b46f6492e5fa300d9ea34
dartmern <72849202+dartmern@users.noreply.github.com>
2022-02-01 08:02:25 -0800

    Update `interaction_check` in paginator

--- jishaku/shim/paginator_200.py
+++ jishaku/shim/paginator_200.py
@@ -257,7 +257,7 @@ class PaginatorInterface(ui.View):  # pylint: disable=too-many-instance-attribut
             else:
                 await self.message.edit(view=None)
 
-    async def interaction_check(self, interaction: discord.Interaction):
+    async def interaction_check(self, item: discord.ui.Item, interaction: discord.Interaction):
         """Check that determines whether this interaction should be honored"""
         return not self.owner or interaction.user.id == self.owner.id
 
